db4c1ace1ea63ae9ee5920dd173bc2915ef3db94
Jamshidbek <jamshidbek1805@gmail.com>
2023-01-07 23:47:43 +0500

    added function for body

--- storage/postgres/body.go
+++ storage/postgres/body.go
@@ -20,77 +20,77 @@ func NewBodyRepo(db *sqlx.DB) repo.BodyRepoInterface {
 	}
 }
 
-func (r *bodyRepasitory) CreateBody(body *pb.CreateBody)(*pb.Body,error){
-	newBody:=pb.Body{}
-	query:=`INSERT INTO body (name,created_at) VALUES($1,$2) RETURNING id,name,created_at`
-	err:=r.db.QueryRow(query,body.Name,time.Now().UTC()).Scan(
+func (r *bodyRepasitory) CreateBody(body *pb.CreateBody) (*pb.Body, error) {
+	newBody := pb.Body{}
+	query := `INSERT INTO body (name,created_at) VALUES($1,$2) RETURNING id,name,created_at`
+	err := r.db.QueryRow(query, body.Name, time.Now().UTC()).Scan(
 		&newBody.Id,
 		&newBody.Name,
 		&newBody.Created_at,
 	)
-	if err!=nil{
-		return nil,err
+	if err != nil {
+		return nil, err
 	}
-	return &newBody,nil
+	return &newBody, nil
 }
 
-func (r *bodyRepasitory) UpdateBody(upBody *pb.UpdateBody)(*pb.Body,error){
-	body:=pb.Body{}
-	query:=`UPDATE body SET name=$2,updated_at=$3 where deleted_at is null and id=$1 RETURNING id,name,created_at,updated_at`
-	err:=r.db.QueryRow(query,upBody.Id,upBody.Name,time.Now().UTC()).Scan(
+func (r *bodyRepasitory) UpdateBody(upBody *pb.UpdateBody) (*pb.Body, error) {
+	body := pb.Body{}
+	query := `UPDATE body SET name=$2,updated_at=$3 where deleted_at is null and id=$1 RETURNING id,name,created_at,updated_at`
+	err := r.db.QueryRow(query, upBody.Id, upBody.Name, time.Now().UTC()).Scan(
 		&body.Id,
 		&body.Name,
 		&body.Created_at,
 		&body.Updated_at,
 	)
-	if err!=nil{
-		return nil,err
+	if err != nil {
+		return nil, err
 	}
-	return &body,nil
+	return &body, nil
 }
 
-func (r *bodyRepasitory) GetBody(id string)(*pb.Body,error){
-	body:=pb.Body{}
+func (r *bodyRepasitory) GetBody(id string) (*pb.Body, error) {
+	body := pb.Body{}
 	var updated_at sql.NullTime
-	query:=`SELECT id,name,created_at,updated_at from body where deleted_at is null and id=$1`
-	err:=r.db.QueryRow(query,id).Scan(
+	query := `SELECT id,name,created_at,updated_at from body where deleted_at is null and id=$1`
+	err := r.db.QueryRow(query, id).Scan(
 		&body.Id,
 		&body.Name,
 		&body.Created_at,
 		&updated_at,
 	)
-	if err!=nil{
-		return nil,err
+	if err != nil {
+		return nil, err
 	}
-	if updated_at.Valid{
-		body.Updated_at=updated_at.Time.String()
+	if updated_at.Valid {
+		body.Updated_at = updated_at.Time.String()
 	}
-	return &body,nil
+	return &body, nil
 }
 
-func (r *bodyRepasitory) GetAllBody()([]*pb.Body,error){
+func (r *bodyRepasitory) GetAllBody() ([]*pb.Body, error) {
 	var bodies []*pb.Body
-	query:=`SELECT id,name,created_at,updated_at where deleted_at is null`
-	rows,err:=r.db.Query(query)
-	if err!=nil {
-		return nil,err
+	query := `SELECT id,name,created_at,updated_at where deleted_at is null`
+	rows, err := r.db.Query(query)
+	if err != nil {
+		return nil, err
 	}
-	for rows.Next(){
+	for rows.Next() {
 		var updated_at sql.NullTime
-		body:=pb.Body{}
-		err:=rows.Scan(
+		body := pb.Body{}
+		err := rows.Scan(
 			&body.Id,
 			&body.Name,
 			&body.Created_at,
 			&updated_at,
 		)
-		if err!=nil{
-			return nil,err
+		if err != nil {
+			return nil, err
 		}
-		if updated_at.Valid{
-			body.Updated_at=updated_at.Time.String()
+		if updated_at.Valid {
+			body.Updated_at = updated_at.Time.String()
 		}
 		bodies = append(bodies, &body)
 	}
-	return bodies,nil
+	return bodies, nil
 }
